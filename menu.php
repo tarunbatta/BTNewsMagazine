c78205f5a1760a5cf273c9b163c362c2357d6b50
Tarun Batta <tarunbatta@gmail.com>
2015-07-04 14:03:23 -0700

    fixed menu collapse issue for small devices

--- menu.php
+++ menu.php
@@ -2,7 +2,7 @@
 	<div class="container-fluid">
 		<!-- Brand and toggle get grouped for better mobile display -->
 		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
 				<span class="sr-only">Toggle navigation</span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
@@ -11,7 +11,7 @@
 			<a class="navbar-brand" href='<?php echo home_url(); ?>'><?php bloginfo('name'); ?></a>
 		</div>
 		<!-- Collect the nav links, forms, and other content for toggling -->
-		<div class="collapse navbar-collapse">
+		<div id="navbar" class="collapse navbar-collapse">
 			<?php 
 				$args = array(
 					'theme_location' => 'primary',
