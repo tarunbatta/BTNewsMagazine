1ca0c90f5ad29e5c326800a0cc02b247dfe5253b
Tarun Batta <tarunbatta@gmail.com>
2015-07-04 12:06:22 -0700

    updated aside and gallery template

--- article-aside.php
+++ article-aside.php
@@ -1,6 +1,8 @@
 <article class="well">
 	<p>
-		<a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> @ <?php the_time('F jS, Y g:i a'); ?> 
+		<a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> | <?php the_time('F jS, Y g:i a'); ?> 
 	</p>
-	<?php the_content(); ?>
+	<div style="text-align: justify;">
+		<?php the_content(); ?>
+	</div>
 </article>
--- article-gallery.php
+++ article-gallery.php
@@ -1,4 +1,7 @@
 <article>
-	<h2><?php the_title(); ?></h2>
-	<?php the_content(); ?>
+	<h3>
+		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+	</h3>
+	<?php get_template_part('postMeta'); ?>
+	<?php echo get_the_excerpt(); ?>
 </article>
