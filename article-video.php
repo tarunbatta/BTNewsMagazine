3ecdad07f052dbe81ce57d32ccce6d547e781e1a
Tarun Batta <tarunbatta@gmail.com>
2015-07-04 13:55:55 -0700

    updated video and audio templates

--- article-video.php
+++ article-video.php
@@ -1,16 +1,9 @@
 <article>
-	<?php 
-	if (has_post_thumbnail()) {
-	?>
-		<div>
-			<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('image_thumbnail'); ?></a>
-		</div>
-	<?php
-	}
-	?>
-	<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-	<p><?php the_time('F jS, Y g:i a'); ?> | By <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> | Posted in <?php getPostCategories() ?></p>
-	<p>
-		<?php echo get_the_excerpt(); ?>
-	</p>
+	<h3>
+		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+	</h3>
+	<?php get_template_part('postMeta'); ?>
+	<div style="text-align: justify;">
+		<?php the_content(); ?>
+	</div>
 </article>
